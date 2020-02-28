084fa91fc8c102ed4f2fe2e624629d103097d591
ralzinov <ralzinov@iponweb.net>
2020-02-28 17:11:42 +0300

    wip

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,8 +3,37 @@ import {map, filter} from 'rxjs/operators';
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {marker as i18n} from '@biesbjerg/ngx-translate-extract-marker';
+import {BehaviorSubject, combineLatest} from 'rxjs';
 
 const WORLD_CONSTANT = i18n('world');
+const HOST_MESSAGE_PLURAL = i18n(`
+    {hostGender, select,
+        male {
+            {guestsLength, plural,
+                =0    { {hostName} does not give a party.}
+                one   { {hostName} invites {mainGuest} to <b>his</b> party.}
+                =2    { {hostName} invites {mainGuest} and one other person to <b>his</b> party.}
+                other { {hostName} invites {mainGuest} and {otherGuestsLength} other people to <b>his</b> party.}
+            }
+        }
+        female {
+            {guestsLength, plural,
+                =0    { {hostName} does not give a party.}
+                one   { {hostName} invites {mainGuest} to <b>her</b> party.}
+                =2    { {hostName} invites {mainGuest} and one other person to <b>her</b> party.}
+                other { {hostName} invites {mainGuest} and {otherGuestsLength} other people to <b>her</b> party.}
+            }
+        }
+        other {
+            {guestsLength, plural,
+                =0    { {hostName} does not give a party.}
+                one   { {hostName} invites {mainGuest} to <b>their</b> party.}
+                =2    { {hostName} invites {mainGuest} and one other person to <b>their</b> party.}
+                other { {hostName} invites {mainGuest} and {otherGuestsLength} other people to <b>their</b> party.}
+            }
+        }
+    }
+`);
 
 interface IHostProfile {
     name: string;
@@ -36,6 +65,18 @@ export class ListComponent implements OnInit {
         map((value) => ({value}))
     );
 
+
+    listChange$ = new BehaviorSubject<void>(null);
+    hostPluralMessage$ = combineLatest(this.translate.onLangChange, this.listChange$).pipe(
+        map(() => this.translate.instant(HOST_MESSAGE_PLURAL, {
+            hostGender: this.host.gender,
+            hostName: this.host.name,
+            guestsLength: this.guests.length,
+            mainGuest: this.guests[0],
+            otherGuestsLength: this.guests.length - 1
+        }))
+    );
+
     constructor(protected translate: TranslateService) {
         this.translate.instant('test');
     }
@@ -44,23 +85,22 @@ export class ListComponent implements OnInit {
         return this.translate.currentLang;
     }
 
-    get hostName() {
-        return this.host.name;
-    }
-
     ngOnInit() {
         this.param$.subscribe(console.log);
     }
 
     setHost(host: IHostProfile): void {
         this.host = host;
+        this.listChange$.next();
     }
 
     add() {
         this.guests.push(`${faker.name.firstName()} ${faker.name.lastName()}`);
+        this.listChange$.next();
     }
 
     remove() {
         this.guests = this.guests.slice(1, this.guests.length);
+        this.listChange$.next();
     }
 }
